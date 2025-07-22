ffb90f53c48d78ff4566406e3c306af18576fc0d
Ahmed Ajanovic <ajanovicahmed11@hotmail.com>
2025-07-22 14:52:17 +0200

    initial commit

---
+++ src/test/java/dev/ahmedajan/mediconnect/MediConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ahmedajan.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
